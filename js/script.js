8bfc25786cea07b2b7583ad9b57b78dd343dec7b
mikelubs <michaelubembe@gmail.com>
2017-04-12 17:27:40 +0300

    indent the code

--- js/script.js
+++ js/script.js
@@ -3,23 +3,23 @@ function myFunction() {
     var y = parseInt(x);
     var text = "";
     var z;
-for (z = 1; z <= y; z++) {
-  if (z%3 == 0 && z%5 == 0) {
-    text += "ping-pong" + "<br>";
-  }
+    for (z = 1; z <= y; z++) {
+      if (z%3 == 0 && z%5 == 0) {
+        text += "ping-pong" + "<br>";
+      }
 
-    else if (z%3 == 0) {
-      text += "ping" + "<br>";
+        else if (z%3 == 0) {
+          text += "ping" + "<br>";
+        }
+
+        else if (z%5 == 0) {
+          text += "pong" + "<br>";
+        }
+
+        else {
+          text += "" + z + "<br>";
+        }
     }
 
-    else if (z%5 == 0) {
-      text += "pong" + "<br>";
-    }
-
-    else {
-      text += "" + z + "<br>";
-    }
-}
-
-document.getElementById("demo").innerHTML = text;
+    document.getElementById("demo").innerHTML = text;
 }
